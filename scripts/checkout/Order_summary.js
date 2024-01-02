73a1ea5ad939f31d3df8fdbf7c4510e568c045eb
Ace Tech <blessedlyrics11@gmail.com>
2024-01-02 20:23:45 +0100

    I made payment summary interactive.

--- scripts/checkout/Order_summary.js
+++ scripts/checkout/Order_summary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/Delivery_options.js";
+import { renderPaymentSummary } from "./Payment_summary.js";
 
 
 export function renderOrderSummary() {
@@ -118,6 +119,8 @@ export function renderOrderSummary() {
     container.remove();
 
     checkoutQuantity();
+
+    renderPaymentSummary();
     })
   })
 
@@ -165,6 +168,8 @@ export function renderOrderSummary() {
     
           saveToCart(productId, valueOfItem);
         }
+
+        renderPaymentSummary();
       })
     })
   }
@@ -175,6 +180,8 @@ export function renderOrderSummary() {
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+
+      renderPaymentSummary();
     });
   });
 }
