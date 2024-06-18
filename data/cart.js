017679dc12210c3bb3fccadff3e50c052bf7f16d
Ace Tech <blessedlyrics11@gmail.com>
2024-06-18 12:20:49 +0100

    starting over.

--- data/cart.js
+++ data/cart.js
@@ -5,7 +5,7 @@ loadFromStorage();
 export function loadFromStorage() {
   cart = JSON.parse(localStorage.getItem('cart'));
 
-if (!cart) {
+  if (!cart) {
   cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
